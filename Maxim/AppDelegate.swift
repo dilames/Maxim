e943ef5a5e01e7c9bf436aeb739b5354d4cc6335
Andrew Chersky <andrew.chersky@gmail.com>
2019-05-07 01:52:18 +0300

    Initial Commit

---
+++ Maxim/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Maxim
+//
+//  Created by Andrew Chersky on 5/7/19.
+//  Copyright © 2019 Andrew Chersky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Maxim/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Maxim
+//
+//  Created by Andrew Chersky on 5/7/19.
+//  Copyright © 2019 Andrew Chersky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
